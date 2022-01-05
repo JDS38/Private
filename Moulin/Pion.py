3bdc884402be763d0cee5d03148707d592afda5a
Surimi115 <60627682+Surimi115@users.noreply.github.com>
2022-01-05 17:18:54 +0100

    nettoyage du code

--- Moulin/Pion.py
+++
@@ -1,7 +0,0 @@
-class Pion:
-
-    def init(self, x, y, couleur):
-        self.posX = x
-        self.posY = y
-        self.couleur = couleur
-        self.InMoulin = False
--- Moulin/main.py
+++ Moulin/main.py
@@ -1,39 +1,79 @@
-"""
-Usage:
-======
-    main.py corp principale de notre jeu du moulin
+class Damier:
 
-"""
-
-__authors__ = "Jean-Daniel SPADAZZI & Tedi KPOGNON & GAY Guillaume "
-__date__ = "2021/11/17"
-__version__ = "1.1"
-
-# L'importation de l’ensemble des éléments du paquet tkinter :
-
-from tkinter import *
-
-import Joueur
-import Intersection
-
-# Variable globale
-
-# global Nbrpion1, Nbrpion2, Restepion1, Restepion2, grille, grillepast
-
-'''class Intersection:
-    def __init__(self,nom,x,y):
-        self.nom = nom
-        self.x = x
-        self.y = y
-        self.vide = True'''
-
-'''class Joueur:
     def __init__(self):
-        self.Nbrpion = 9 #Définition attribut nombre de pion à poser
-        self.Restepion = 9 #Définition attribut nombre de pion restant'''
+        self.area = [[0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 1, 0, 0, 1, 0, 0, 1],
+                     [0, 0, 1, 0, 1, 0, 1, 0],
+                     [0, 0, 0, 1, 1, 1, 0, 0],
+                     [0, 1, 1, 1, 0, 1, 1, 1],
+                     [0, 0, 0, 1, 1, 1, 0, 0],
+                     [0, 0, 1, 0, 1, 0, 1, 0],
+                     [0, 1, 0, 0, 1, 0, 0, 1]]
+
+    def str(self):
+        res = ""
+        for i in range(len(self.area)):
+            for j in range(len(self.area[i])):
+                res += str(self.area[i][j]) + " "
+            res += "\n"
+        return res
 
 
+class Joueur:
 
+    def __init__(self, couleur, pionsRestants):
+        self.couleur = couleur
+        self.pionRestants = pionsRestants
+        self.listePionsEnJeu = []
+
+    def pose(self, x, y, ListIntersections, grille):
+        for inter in ListIntersections:
+            if x == inter[0] and y == inter[1] and inter.occupee == False:
+                pion = Pion(x, y, self.couleur)
+                grille[x][y] = pion
+                self.listePionsEnJeu.append(pion)
+                self.pionRestants -= 1
+
+
+class Pion:
+
+    def __init__(self, x, y, couleur):
+        self.posX = x
+        self.posY = y
+        self.couleur = couleur
+        self.InMoulin = False
+
+
+class Intersection:
+    def __init__(self, nom, x, y):
+        self.Nom = nom
+        self.X = x
+        self.Y = y
+        self.Occupee = False
+        self.Voisins = {'inter1': ('inter2', 'inter10'),
+                        'inter2': ('inter1', 'inter3', 'inter5'),
+                        'inter3': ('inter2', 'inter15'),
+                        'inter4': ('inter5', 'inter11'),
+                        'inter5': ('inter2', 'inter4', 'inter6', 'inter8'),
+                        'inter6': ('inter5', 'inter14'),
+                        'inter7': ('inter8', 'inter12'),
+                        'inter8': ('inter5', 'inter7', 'inter9'),
+                        'inter9': ('inter8', 'inter13'),
+                        'inter10': ('inter1', 'inter11', 'inter22'),
+                        'inter11': ('inter4', 'inter10', 'inter12', 'inter19'),
+                        'inter12': ('inter7', 'inter11', 'inter16'),
+                        'inter13': ('inter9', 'inter14', 'inter18'),
+                        'inter14': ('inter6', 'inter13', 'inter15', 'inter21'),
+                        'inter15': ('inter3', 'inter14', 'inter24'),
+                        'inter16': ('inter12', 'inter17'),
+                        'inter17': ('inter16', 'inter18', 'inter20'),
+                        'inter18': ('inter13', 'inter17'),
+                        'inter19': ('inter11', 'inter20'),
+                        'inter20': ('inter17', 'inter19', 'inter21', 'inter23'),
+                        'inter21': ('inter14', 'inter20'),
+                        'inter22': ('inter10', 'inter23'),
+                        'inter23': ('inter20', 'inter22', 'inter24'),
+                        'inter24': ('inter15', 'inter23')}
 
 
 def phase_jeu(status):
@@ -47,8 +87,9 @@ def phase_jeu(status):
             print()
             print("Phase de Pose Joueur vs Joueur")
 
-            # while Nbrpion2 > 0 :
-            # Pose
+            #while joueur2.pionRestants > 0 :
+                #coo = input("\n Joueur 1 Entrez les coordonnées de pose sous la forme x,y: ")
+                #joueur1.pose()
 
             # Moulin?
 
@@ -103,6 +144,8 @@ def phase_jeu(status):
 
 def init_game():
 
+    grille = Damier()
+
     inters = []
     inters.append(Intersection("inter1", 1, 1))
     inters.append(Intersection("inter2", 4, 1))
@@ -129,33 +172,23 @@ def init_game():
     inters.append(Intersection("inter23", 4, 7))
     inters.append(Intersection("inter24", 7, 7))
 
-    print("Choix du mode de jeu :")
+    print("\nChoix du mode de jeu :")
     print("1. Humain vs Humain")
     print("2. Humain vs Ordinateur")
-    mode = int(input("Indiquez votre choix : "))
-    if mode == 1:
+    mode = input("Indiquez votre choix : ")
+    if mode == '1':
         phase_jeu(2)  # phase de pose j vs j
 
-    if mode == 2:
+    if mode == '2':
         phase_jeu(20)  # phase de pose j vs ordinateur
-    if mode != 1 and mode != 2:
+    if mode != '1' and mode != '2':
         print("Choix impossible, entrez un choix possible")
         init_game()
 
-#------------------------------------------------------------
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
     joueur1 = Joueur("noir", 9)
     joueur2 = Joueur("blanc", 9)
 
-    print()
     phase_jeu(1)
-
-'''    # Définition des deux joueurs
-    un = Joueur()
-    deux = Joueur()
-
-    print("J1 : Nombre de pion à poser =", un.Nbrpion, "Nombre de pion restant =", deux.Restepion)
-    print("J2 : Nombre de pion à poser =", un.Nbrpion, "Nombre de pion restant =", deux.Restepion)'''
